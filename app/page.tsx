a1ca9644730fa1a402fd73ae5b1f5b53c7306796
Thiago <tmelo387@gmail.com>
2025-04-08 20:32:29 -0300

    fix: removed unused content

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from './components/atoms/button'
-
 export default function Home() {
-  return (
-    <div className="">
-      <Button color="delete-btn">
-        <h3 className="bg-delete-btn"> teste3</h3>
-      </Button>
-    </div>
-  )
+  return <div className="">Here is the main content</div>
 }
