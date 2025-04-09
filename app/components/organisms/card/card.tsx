28ce2e117f131336dc65a60a52950876dcdf79dc
Thiago <tmelo387@gmail.com>
2025-04-09 15:15:41 -0300

    feat: changed divider color

--- app/components/organisms/card/card.tsx
+++ app/components/organisms/card/card.tsx
@@ -14,14 +14,14 @@ export const Card = ({ title, description }: CardProps) => {
     <button
       type="button"
       id="card"
-      className="p-2 flex flex-col items-center border-2 rounded-2xl h-[200px] cursor-pointer shadow-md hover:shadow-lg transition-shadow duration-200 ease-in-out"
+      className="p-2 flex flex-col items-center border-2 rounded-2xl h-[200px] cursor-pointer"
       onClick={() => {
         onClick()
       }}
     >
       <div className="w-full">
         <h1 className="font-bold text-center">{title}</h1>
-        <div className="border-2 rounded-2xl w-full" />
+        <div className="border-2 rounded-2xl w-full border-purple" />
       </div>
       <div className="pt-4 px-2 text-justify">
         <p className="line-clamp-4">{description}</p>
