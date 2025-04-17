6097cc4057d99612c887687dd703ad91b766f8ce
Thiago <tmelo387@gmail.com>
2025-04-16 22:24:31 -0300

    fix: changed to flex wrap

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,27 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="pb-2 text-2xl">{t('title')}</h1>
-      <div className="grid lg:grid-cols-4 grid-cols-2 px-4 mt-2 gap-4">
+      <div className="flex flex-wrap justify-center w-full px-4 mt-4 gap-8">
+        <Card
+          title="Silver Bullet"
+          description="I've been contributing to Silver Bullet since 2021"
+          onClick={() => router.push('/experiences/teste')}
+        />
+        <Card
+          title="Silver Bullet"
+          description="I've been contributing to Silver Bullet since 2021"
+          onClick={() => router.push('/experiences/teste')}
+        />
+        <Card
+          title="Silver Bullet"
+          description="I've been contributing to Silver Bullet since 2021"
+          onClick={() => router.push('/experiences/teste')}
+        />
+        <Card
+          title="Silver Bullet"
+          description="I've been contributing to Silver Bullet since 2021"
+          onClick={() => router.push('/experiences/teste')}
+        />
         <Card
           title="Silver Bullet"
           description="I've been contributing to Silver Bullet since 2021"
