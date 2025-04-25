31501f09f08b4d498313c95c3f7a1db6820922ce
chucrutes <tmelo387@gmail.com>
2025-04-25 15:33:32 -0300

    chore: allowing origin for mobile

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.0.14']
+}
 const withNextIntl = createNextIntlPlugin('./src/app/i18n/request.ts')
 export default withNextIntl(nextConfig)
